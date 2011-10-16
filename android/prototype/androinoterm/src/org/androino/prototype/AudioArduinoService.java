e2e7a61b665bf4637fffeb26a939bdcecdf3791d
androino2011 <androino2011@gmail.com>
2011-10-16 19:46:32 +0000

    added decoding functions

--- android/prototype/androinoterm/src/org/androino/prototype/AudioArduinoService.java
+++ android/prototype/androinoterm/src/org/androino/prototype/AudioArduinoService.java
@@ -41,6 +41,9 @@ public class AudioArduinoService extends ArduinoService {
 		audioR.startRecording();
 */
 		this.forceStop = false;
+		
+		testDecodeAmplitude();
+		
 		// continuous loop
 		while (true) {
 			// stop if requested
@@ -64,9 +67,9 @@ public class AudioArduinoService extends ArduinoService {
 
 	public void write(String message) {
 		Log.i("ArduinoService::MSG", message);
-		//testRecordAudio();
-		testFrequency = testFrequency + 300;
-		if (testFrequency>3000) testFrequency = 400;
+		testRecordAudio();
+		//testFrequency = testFrequency + 300;
+		//if (testFrequency>3000) testFrequency = 400;
 
 	}
 	public void stopAndClean(){
@@ -108,9 +111,9 @@ public class AudioArduinoService extends ArduinoService {
 				AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT,
 				AUDIO_BUFFER_SIZE, AudioTrack.MODE_STREAM);
 		aT.play();
-		//int nBytes = aT.write(this.testAudioArray, 0, this.testAudioArray.length);
-		byte[] tone = generateTone(this.testFrequency);
-		int nBytes = aT.write(tone, 0, tone.length);
+		int nBytes = aT.write(this.testAudioArray, 0, this.testAudioArray.length);
+		//byte[] tone = generateTone(this.testFrequency);
+		//int nBytes = aT.write(tone, 0, tone.length);
 		aT.stop();
 		aT.release();
 	}
@@ -189,5 +192,120 @@ public class AudioArduinoService extends ArduinoService {
 			//if (counter > 10) break;
 		}
 	}
+
+	private void testDecodeAmplitude() {
+		int AUDIO_BUFFER_SIZE = 4000;//16000;
+		int minBufferSize = AudioTrack.getMinBufferSize(AUDIO_SAMPLE_FREQ, 
+				2, AudioFormat.ENCODING_PCM_16BIT);
+		if (AUDIO_BUFFER_SIZE < minBufferSize) AUDIO_BUFFER_SIZE = minBufferSize;
+				
+		Log.i(TAG, "buffer size:" + AUDIO_BUFFER_SIZE);
+		byte[] audioData = new byte[AUDIO_BUFFER_SIZE];
+
+		// appendDebugInfo("BufferSize", "" + AUDIO_BUFFER_SIZE);
+		AudioRecord aR = new AudioRecord(
+				MediaRecorder.AudioSource.MIC, AUDIO_SAMPLE_FREQ, 2,
+				AudioFormat.ENCODING_PCM_16BIT, AUDIO_BUFFER_SIZE);
+
+		// audio recording
+		aR.startRecording();
+		int nBytes = 0;
+		int index = 0;
+		int counter = 0;
+		this.forceStop = false;
+		// continuous loop
+		String message ="";
+		while (true) {
+			counter++;
+			nBytes = aR.read(audioData, index, AUDIO_BUFFER_SIZE );
+			//Log.v(TAG, "nBytes=" + nBytes);
+			if (nBytes<0) {
+				Log.e(TAG, "read error=" + nBytes);
+				break; //error happened
+			}
+			//int volume = (int)decodeAmplitude(audioData, nBytes);
+			//Log.i(TAG, "volume="+volume+"%");
+			double frequency = decodeFrequency(audioData, nBytes);
+			Log.i(TAG, "freq="+frequency+"Hz");
+			
+			/*
+			int volume = (int)(10.0* decodeAmplitude(audioData, nBytes));
+			message += "," + volume;
+			
+			if (counter >20){
+				counter = 0;
+				Log.i(TAG, "volume=" + message);
+				
+				message="";
+			}
+			*/
+			//String message = "Sampling=" + AUDIO_SAMPLE_FREQ + ":" + "buffer size="  +AUDIO_BUFFER_SIZE + "\n"; 
+			//saveAudioToFile(message, audioData);
+			// stop if requested
+			if (this.forceStop) {
+				break;
+			}
+		}
+		aR.stop();
+		aR.release();
+		Log.i(TAG, "audio recording stoped");
+	}
+	private double decodeAmplitude(byte[] audioData, int nBytes){
+		double volume = 0;
+		ByteBuffer buf = ByteBuffer.wrap(audioData, 0, nBytes);
+		buf.order(ByteOrder.LITTLE_ENDIAN);
+		double NORM_FACTOR = 32768.0;
+
+		int counter=0;
+		double amplitude = 0;
+		while (buf.remaining() >= 2) {
+			counter++;
+			short s = buf.getShort();
+			double value = (double) s;
+			value = value / NORM_FACTOR;
+			if (value<0) value = value* (-1.0);
+			amplitude+=value;
+			//if (counter > 10) break;
+		}
+		volume = 100 * amplitude / counter;
+		Log.v(TAG, "decodeAmplitude():volume="+volume);
+		//sendMessage(volume, 0);
+		return volume;
+	}
+
+	private double decodeFrequency(byte[] audioData, int nBytes){
+		double frequency = 0;
+		ByteBuffer buf = ByteBuffer.wrap(audioData, 0, nBytes);
+		buf.order(ByteOrder.LITTLE_ENDIAN);
+		
+		int counter=0;
+		int signChangeCounter = 0;
+		int sign = 1;
+		while (buf.remaining() >= 2) {
+			counter++;
+			short s = buf.getShort();
+			double value = (double) s;
+			if (value >0){
+				if (sign<0) {
+					signChangeCounter++;
+					sign = 1;
+				}
+			} else { //value <0
+				if (sign>0){
+					signChangeCounter++;
+					sign = -1;
+				}
+			}
+		}
+		int nCycles = signChangeCounter/2;
+		double time = (nBytes/2.0) / AUDIO_SAMPLE_FREQ;
+
+		frequency = nCycles/time;
+		Log.v(TAG, "decodeFrequency():freq="+frequency);
+		//sendMessage(volume, 0);
+		return frequency;
+	}
+	
+	
 
 }
