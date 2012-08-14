1f92c8ef3d5dfdce94ab0b48bdf9caecc999321c
androino2011 <androino2011@gmail.com>
2012-08-14 14:36:46 +0000

    working version: validating logic with electronic circuit

--- runcorre/runcorre.ino
+++ runcorre/runcorre.ino
@@ -13,20 +13,22 @@
 #define S_GAME_ENDS    4
 
 #define CHALLENGE_TIMEOUT  1000
-
+#define BLINK_LED_INTERVAL 500
 
 int STATE = S_READY; //initial state
 
 // internal variables
-unsigned long time;
+unsigned long timeStartChallenge;
+unsigned long timeStartLed;
 int challenge = 1;
+boolean blinkLedState = false;
 
 // pin mapping
 int inputPin = A0;
-int ALedPin = 8;
-int ABuzzerPin = 9;
-int BLedPin = 10;
-int BBuzzerPin = 11;
+int ALedPin = 10;
+int ABuzzerPin = 11;
+int BLedPin = 12;
+int BBuzzerPin = 13;
 
 int led = 13;
 
@@ -35,67 +37,119 @@ int led = 13;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  
+
   // pin intialization
   pinMode(ALedPin, OUTPUT);  
   pinMode(ABuzzerPin, OUTPUT);  
   pinMode(BLedPin, OUTPUT);  
   pinMode(BBuzzerPin, OUTPUT);  
+  debugMessage("RunCorre Version 0.0");
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   stateMachine();
+  //  delay(1000);
 }
 
 // main game state machine 
 void stateMachine(){
-
+  //  Serial.print("stateMachine: STATE="); Serial.println(STATE);
   int response = 0;
   switch (STATE) {
   case S_READY:
     //initialization checks
-    STATE = S_GAME_STARTS;
+    setState(S_GAME_STARTS);
+    //  Serial.print("stateMachine: READY STATE="); Serial.println(STATE);
     break;
   case S_GAME_STARTS:
     // game start tune
     playStartGameTune();
-    STATE = S_CHALLENGE;
+    setState(S_CHALLENGE);
     break;
   case S_CHALLENGE:
     // launch challenge
     launchChallenge();
-    time = millis(); //start timeout counter
-    STATE = S_RESPONSE;
+    setState(S_RESPONSE);
     break;
   case S_RESPONSE:
+    // play challenge
+    playChallenge(challenge);
     // wait for response or timeout
     response = checkResponse();
+    //  Serial.print("stateMachine: response="); Serial.println(response);
     if (response == 1) { // solved, go for next challenge
-      STATE = S_CHALLENGE;
+      setState(S_CHALLENGE);
     };
     if (response == -1) { // timeout or failure, game ends
-      STATE = S_GAME_ENDS;
+      setState(S_GAME_ENDS);
     };
     break;
   case S_GAME_ENDS:
     // you loose tune and back to the beginning
     playEndGameTune();
-    STATE = S_READY;
+    setState(S_READY);
     break;
   }
+  //  Serial.print("stateMachine: >>STATE="); Serial.println(STATE);
+}
+
+void setState(int state){
+  Serial.print("setState: >>STATE="); 
+  Serial.print(STATE);
+  Serial.print(">>");
+  Serial.println(state);
+  STATE = state;
 }
 
 void playStartGameTune(){
+  debugMessage("playStartTune");
   dummyTune();
 }
 void playEndGameTune(){
+  debugMessage("playEndTune");
   dummyTune();
 }
 void playChallenge(int id){
-  dummyTune();
-  debugMessage(String( "playChallenge:" + id));
+  int pin = ALedPin;
+  int buzzer = ABuzzerPin;
+  if (id == 2) {
+    pin = BLedPin;
+    buzzer = BBuzzerPin;
+  }
+  playASynchro(pin, buzzer);
 }
+
+void playASynchro(int pin, int buzzer){
+  unsigned long t = millis();
+  long counter =  t-timeStartLed; 
+  if (counter > BLINK_LED_INTERVAL)  {
+    timeStartLed = millis();
+    if ( blinkLedState ) {
+      blinkLedState = false;
+      digitalWrite(pin, LOW);
+      tone(buzzer, 1000);
+    } 
+    else {
+      blinkLedState = true;
+      digitalWrite(pin, HIGH);
+      tone(buzzer, 3000);
+    }   
+  };
+
+}
+
+void playSynchro(int pin, int buzzer){
+  tone(buzzer, 3000, 2000);
+  for (int i=0; i<10; i++){
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(200);
+  }
+}
+
+
 void dummyTune(){
   digitalWrite(led, HIGH);
   delay(1000);
@@ -107,7 +161,19 @@ void dummyTune(){
 void launchChallenge(){
   long n = random(1,3);
   challenge = n;
-  playChallenge(challenge);
+  Serial.print("launchChallenge: challenge="); 
+  Serial.println(challenge);
+  timeStartChallenge = millis(); //start timeout counter
+  timeStartLed = timeStartChallenge; // led counter
+  blinkLedState = true;
+  resetPlayChallenge();
+}
+
+void resetPlayChallenge(){
+  for(int i=0; i<4; i=i+2){
+    digitalWrite(ALedPin +i, LOW);  
+    noTone(ABuzzerPin +i);
+  }
 }
 
 int checkResponse(){
@@ -115,17 +181,18 @@ int checkResponse(){
   // 0: no response
   // 1: ok
   // -1: timeout or failure
-  if (isTimeOut) return -1;
+  //  if (isTimeOut) return -1;
   int response = readUserInput();
   if (response == challenge) {
     debugMessage(String( "checkResponse:" + response));
     return 1;
-  } else 
+  } 
+  else 
     return 0;
 }
 boolean isTimeOut(){
   unsigned long t = millis();
-  long counter =  t-time; 
+  long counter =  t-timeStartChallenge; 
   if (counter > CHALLENGE_TIMEOUT) return true;
   else false;
 }
@@ -140,14 +207,14 @@ int readUserInput(){
     meanValue += analogRead(inputPin); // 0-1023
   }
   meanValue = meanValue/nSamples;
-  
-  if ( 100 < meanValue && meanValue < 500 ) {
-    userInput = 1;
-    debugMessage(String( "userInput:" + userInput));
- }
-  if ( 500 < meanValue && meanValue < 900 ) {
+  //  Serial.print("readUserInput: meanValue="); Serial.println(meanValue);
+  if ( 80 < meanValue && meanValue < 500 ) {
     userInput = 2;
-    debugMessage(String( "userInput:" + userInput));
+    debugMessage("userInput:2");
+  }
+  if ( 500 < meanValue && meanValue < 900 ) {
+    userInput = 1;
+    debugMessage("userInput:1");
   }
   return userInput;  
 }
@@ -155,3 +222,4 @@ int readUserInput(){
 void debugMessage(String message){
   Serial.println(message);
 }
+
